46618d396db99dfb1a6da0c8c43780abcc86e031
Suraj4830 <dacharajusuraj@gmail.com>
2024-09-18 12:45:18 +0530

    SURAJD001 - Created Hello World Program

---
+++ src/main/java/com/example/javamavenjunithelloworld/Suraj.java
@@ -0,0 +1,8 @@
+// src/main/java/com/example/javamavenjunithelloworld/John.java
+package com.example.javamavenjunithelloworld;
+
+public class Suraj {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
