84fd71c61cb0a361b3d1693721fec2142c816b71
Suraj4830 <dacharajusuraj@gmail.com>
2024-09-18 12:53:54 +0530

    SURAJD0001 - Wrote the code for find the area of the circle

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,11 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    public void suraj(){
+        System.out.println("I am Suraj");
+    }
+
+    public double areaOfCircle(double radius){
+        return Math.PI*radius*radius;
+    }
+}
